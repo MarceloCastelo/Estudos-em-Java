9c5d751758d03219930e94c14b5eb279a3976fcb
Marcelo Castelo Branco <111468803+MarceloCastelo@users.noreply.github.com>
2023-04-14 14:22:50 -0300

    Okay

--- Java/Revisão_Prova_POO/App/src/Aluno.java
+++ Java/Revisão_Prova_POO/App/src/Aluno.java
@@ -1,37 +1,53 @@
+
 public class Aluno {
 
     //Atributos
     private String nome;
-    private double nota;
-
-    //Método Construtor
-    public Aluno(String nome, double nota) {
+    private double nota01;
+    private double nota02;
+    private double nota03;
+    private double nota04;
+    
+    //Construtor
+    public Aluno (String nome, double nota01, double nota02, double nota03, double nota04){
         this.nome = nome;
-        this.nota = nota;
-}
-
-    //Get e Set
-
-    //Recebe o valor da variável
+        this.nota01 = nota01;
+        this.nota02 = nota02;
+        this.nota03 = nota03;
+        this.nota04 = nota04;
+    }
+    
+    //Get e set
     public String getNome() {
         return nome;
     }
-
-    public double getNota() {
-        return nota;
-    }
-
-
-    //Altera o valor da variável
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public void setNota(double nota) {
-        this.nota = nota;
+    public double getNota01() {
+        return nota01;
     }
-
-
+    public double getNota02() {
+        return nota02;
+    }
+    public double getNota03() {
+        return nota03;
+    }
+    public double getNota04() {
+        return nota04;
+    }
+    public void setNota01(double nota01) {
+        this.nota01 = nota01;
+    }
+    public void setNota02(double nota02) {
+        this.nota02 = nota02;
+    }
+    public void setNota03(double nota03) {
+        this.nota03 = nota03;
+    }
+    public void setNota04(double nota04) {
+        this.nota04 = nota04;
+    }
 
 
 
